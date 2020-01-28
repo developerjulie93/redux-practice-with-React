4f28a3b1fb70031d4e13d186135295754b67dc92
Juhee Kang <56218979+developerjulie93@users.noreply.github.com>
2020-01-27 23:47:28 -0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './App.css';
+import Counter from './components/Counter';
+
+const App=()=>{
+  return <Counter number={0} />
+};
+
+export default App;
---
+++ src/components/Todos.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const Todos=()=>{
+    return(
+        <div>
+            <input />
+            <button>Submit</button>
+        </div>
+    );
+};
+export default Todos;
