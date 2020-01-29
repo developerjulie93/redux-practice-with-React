cc9ca052aa3f28244be02b176119720b00f22660
Juhee Kang <56218979+developerjulie93@users.noreply.github.com>
2020-01-29 00:22:45 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,15 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import counter from './modules/counter';
 
-const store = createStore(counter);
+const store = createStore(counter,composeWithDevTools());
 ReactDOM.render(
 <Provider store={store}>
-<App />
+    <App />
 </Provider>, 
 document.getElementById('root'),
 );
